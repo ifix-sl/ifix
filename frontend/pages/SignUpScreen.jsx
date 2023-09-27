d07d7034f5e2a2cfc9d94fa5e8a6b02861ec961d
Dan Niles <56271899+dan-niles@users.noreply.github.com>
2023-09-27 18:04:53 +0530

    Add inputs to sign up screen

--- frontend/pages/SignUpScreen.jsx
+++ frontend/pages/SignUpScreen.jsx
@@ -1,21 +1,51 @@
 import { View, Text } from "react-native";
 import { useState } from "react";
-import { SafeAreaView, StyleSheet, TextInput } from "react-native";
+import { SafeAreaView, StyleSheet } from "react-native";
+import { Button, TextInput } from "react-native-paper";
 
 const SignUpScreen = ({ navigation }) => {
 	const [email, setEmail] = useState("");
+	const [phone, setPhone] = useState("");
 	const [password, setPassword] = useState("");
 
 	return (
 		<SafeAreaView>
-			<TextInput onChangeText={setEmail} value={email} />
-			<TextInput onChangeText={setPassword} value={password} />
-			{/* <TextInput
-				onChangeText={onChangeNumber}
-				value={number}
-				placeholder="useless placeholder"
-				keyboardType="numeric"
-			/> */}
+			<View className="mt-12 mb-0">
+				<Text className="text-center text-5xl m-0">iFix</Text>
+			</View>
+			<View className="p-5 pt-8 mt-3">
+				<TextInput
+					className="mt-5"
+					label="Email"
+					mode="outlined"
+					value={email}
+					placeholder="Enter your email"
+					onChangeText={(text) => setEmail(text)}
+				/>
+				<TextInput
+					className="mt-5"
+					label="Phone Number"
+					mode="outlined"
+					value={phone}
+					keyboardType="numeric"
+					maxLength={9}
+					placeholder="Enter your phone number"
+					onChangeText={(text) => setPhone(text)}
+					left={<TextInput.Affix text="+94" />}
+				/>
+				<TextInput
+					className="mt-5"
+					label="Password"
+					mode="outlined"
+					secureTextEntry={true}
+					value={password}
+					placeholder="Enter your password"
+					onChangeText={(text) => setPassword(text)}
+				/>
+				<Button mode="contained" className="mt-5 py-2">
+					Sign Up
+				</Button>
+			</View>
 		</SafeAreaView>
 	);
 };
